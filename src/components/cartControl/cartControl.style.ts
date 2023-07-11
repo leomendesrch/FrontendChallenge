ce34eec87b96636b50c26e1541ac0f6cd38a7683
Leonardo Mendes <leomendesrch2005@gmail.com>
2023-07-11 10:54:51 -0300

    feat: cartIcon on click redirect to cart page

--- src/components/cartControl/cartControl.style.ts
+++ src/components/cartControl/cartControl.style.ts
@@ -2,16 +2,18 @@
 import styled from 'styled-components'
 
 export const CartCount = styled.span`
-  width: 17px;
-  height: 17px;
+  width: 18px;
+  height: 18px;
   background-color: var(--delete-color);
   border-radius: 17px;
   color: white;
-  padding: 0px 5px;
+  padding: 1px 5px;
   margin-left: -10px;
   font-size: 11px;
 `
 
-export const Container = styled.div`
-
+export const Container = styled.button`
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
 `
--- src/components/cartControl/cartControl.tsx
+++ src/components/cartControl/cartControl.tsx
@@ -2,12 +2,18 @@
 import { useLocalStorage } from "@/hooks/useLocalStorage";
 import { CartIcon } from "../icons/cartIcon";
 import { CartCount, Container } from "./cartControl.style";
+import { useRouter } from "next/navigation";
 
 export function CartControl(){
   const {value} = useLocalStorage('cart-items', [])
-  
+  const router = useRouter()
+
+  const handleNavigateToCart = () => {
+    router.push('/cart')
+  }
+
   return(
-    <Container>
+    <Container onClick={handleNavigateToCart}>
       <CartIcon />
       {value.length > 0 && <CartCount>{value.length}</CartCount>}
     </Container>
